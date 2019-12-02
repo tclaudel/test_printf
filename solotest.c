d5d709e9c4e0843ded7ab22172b16184a24ccd33
Thomas Claudel <tclaudel@z2r1p3.le-101.fr>
2019-12-02 11:18:15 +0100

    path

--- solotest.c
+++ solotest.c
@@ -6,12 +6,12 @@
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/08 10:54:18 by tclaudel     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/20 14:03:14 by tclaudel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/02 08:40:36 by tclaudel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "../libftprintf.h"
 
 #include <stdio.h>
 #include <limits.h>
@@ -19,7 +19,7 @@
 int		main(void)
 {
 	dprintf(1, "\ntesting\t: %s\n", "\"%3i\", -1");
-	if ((printf("|%-3i|\n", -1)) == (ft_printf("|%-3i|\n", -1)))
+	if ((printf("|%*.10x|\n", -45, 200)) == (ft_printf("|%*.10x|\n", -45, 200)))
 		puts("SUCCEED\n");
 	else
 		puts("ERROR\n");
